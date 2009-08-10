a53937205f37c7f5d921e460de75067cee571290
tom <tom@zwizwa.be>
2009-08-10 12:45:00 +0000

    cleanup

--- task/task.c
+++ task/task.c
@@ -22,26 +22,16 @@
 #endif
 
 
-/* Resumable continuations only need a base pointer and a setjmp to
-   unwind the stack.  */
-
-
 static void default_free(ck *ck) {
     free(ck->segment);
     free(ck);
 }
-
-/* Jump to primitive invocation point. */
 static void default_jump(ck_manager *m) {
     longjmp(m->prompt, 1);
 }
-
-/* Data conversion. */
 static void *default_dont_convert(ck_manager *m, void *x) { 
-    
     return x; 
 }
-
 ck_manager *ck_manager_new(void) {
     ck_manager *x = malloc(sizeof(*x));
     x->free = default_free;
@@ -51,9 +41,13 @@ ck_manager *ck_manager_new(void) {
     x->base      = NULL; // filled in on first invoke
     return x;
 }
+ck *ck_new(ck_manager *ck_manager) {
+    ck *ck = malloc(sizeof*ck);
+    ck->manager = ck_manager;
+    return ck;
+}
 
-
-// invoke continuation
+/* HOST SIDE */
 static void resume(ck *_ck, void *base) {
     thread_static ck *ck; ck = _ck;// variable not on C stack.
 
@@ -69,7 +63,6 @@ static void resume(ck *_ck, void *base) {
        optimized away. */
     longjmp(ck->resume, (int)((long)reserve));
 }
-
 void ck_invoke(ck_manager *m, ck_start fn, ck **ck, void **value) {
     void *base;
     if (!setjmp(m->prompt)) {
@@ -89,16 +82,8 @@ void ck_invoke(ck_manager *m, ck_start fn, ck **ck, void **value) {
     return;
 }
 
-ck *ck_new(ck_manager *ck_manager) {
-    ck *ck = malloc(sizeof*ck);
-    ck->manager = ck_manager;
-    return ck;
-}
 
-
-
-
-// create continuation
+/* C TASK SIDE */
 static void suspend(ck_manager *m) {
     ck *ck = m->ck_new = ck_new(m);
     if (0 == setjmp(ck->resume)) {
@@ -114,10 +99,14 @@ static void suspend(ck_manager *m) {
         exit(1); // not reached
     }
 }
-
-
 void* ck_yield(ck_manager *m, void *value) {
     m->channel = value;
     suspend(m);
     return m->channel;
 }
+
+
+
+
+
+
