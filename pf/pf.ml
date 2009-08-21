32c5e772dc4a368daa2c6fe92f401389e485b341
tom <tom@zwizwa.be>
2009-08-21 09:56:08 +0000

    Ocaml examples

--- pf/pf.ml
+++ pf/pf.ml
@@ -1,4 +1,4 @@
-(* Code is nonlinear. *)
+(* Code. *)
 
 type sub = 
     Prim  of prim
@@ -11,7 +11,7 @@ and result =
     Success of stack
   | Error
 
-(* State is linear. *)
+(* State. *)
 
 and datum =
     Number of int
@@ -33,9 +33,7 @@ and state =
 ;;
 
 
-(* Primitive code interpreter.  Note: Prim can't have a type: state ->
-   state (no GADTs in OCaml), so Prim consists of tags which are
-   interpreted explicitly. *)
+(* Code primitives. *)
 
 let call a  =
   match a with
@@ -72,9 +70,21 @@ let run code =
     loop (State(Empty, Frame(code, Done)))
 ;;
 
-(* EXAMPLE:
+(* EXAMPLES:
 
-# run (Seq (Quote (Number 123), Prim Dup)) ;;
-- : result = Success (Push (Number 123, Push (Number 123, Empty)))
+Execute the code "123 dup"
+
+  # run (Seq (Quote (Number 123), Prim Dup)) ;;
+  - : result = Success (Push (Number 123, Push (Number 123, Empty)))
+
+Define a word `foo'
+
+  # let foo = (Seq (Quote (Number 123), Prim Dup)) ;;
+  val foo : sub = Seq (Quote (Number 123), Prim Dup)
+
+Execute the code "foo 456"
+
+  # run (Seq (foo, Quote (Number 456)));;
+  - : result = Success (Push (Number 456, Push (Number 123, Push (Number 123, Empty))))
 
 *)
