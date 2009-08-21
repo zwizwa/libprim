f3665ce33350bce32761d7a86ef9148eb49789f1
tom <tom@zwizwa.be>
2009-08-21 09:47:11 +0000

    ocaml basic interpreter running

--- pf/pf.ml
+++ pf/pf.ml
@@ -12,6 +12,7 @@ and result =
   | Error
 
 (* State is linear. *)
+
 and datum =
     Number of int
   | Code   of sub 
@@ -32,25 +33,48 @@ and state =
 ;;
 
 
-let doprim a  =
+(* Primitive code interpreter.  Note: Prim can't have a type: state ->
+   state (no GADTs in OCaml), so Prim consists of tags which are
+   interpreted explicitly. *)
+
+let call a  =
   match a with
-      App (Dup, Push(d, p)) -> Success(Push(d,Push(d,p)))
-    | App (Dup, p) -> Error
+      App (Dup, Push(d, stk)) -> Success(Push(d,Push(d,stk)))
+    | App (Drop, Push(d, stk)) -> Success(stk)
     | _ -> Error
 ;;
     
+(* Composite code interpreter step function. *)
 
-let run s =
+let step s =
   match s with
       Halt (res) -> Halt (res)
     | State (stk, Done) -> Halt (Success (stk))
-    | State (stk, Frame (sub, knext)) ->
+    | State (stk, Frame (sub, k)) ->
         match sub with
             Prim (fn) -> 
-              (match doprim(App(fn, stk)) with
+              (match call(App(fn, stk)) with
                    Error -> Halt (Error)
-                 | Success(stack) -> State(stack, knext))
-          | Seq (now, next) -> s
-          | Quote (dat) -> State(Push(dat,stk), knext)
+                 | Success(stack) -> State(stack, k))
+          | Quote (dat) -> State(Push(dat,stk), k)
+          | Seq (now, next) -> State(stk, Frame(now, Frame(next, k)))
 ;;
 
+(* Start execution with an empty parameter stack and a continuation
+   frame containing a single subroutine. *)
+
+let run code =
+  let rec loop state =
+    match state with
+        Halt (res) -> res 
+      | s -> loop (step s)
+  in
+    loop (State(Empty, Frame(code, Done)))
+;;
+
+(* EXAMPLE:
+
+# run (Seq (Quote (Number 123), Prim Dup)) ;;
+- : result = Success (Push (Number 123, Push (Number 123, Empty)))
+
+*)
