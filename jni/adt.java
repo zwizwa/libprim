fd11007bf9ffeb207a9b6ef7ace676fce9b035cd
tom <tom@zwizwa.be>
2009-12-07 15:21:56 +0000

    ast.java eval()

--- jni/adt.java
+++ jni/adt.java
@@ -6,7 +6,22 @@ import java.lang.reflect.*;
 
 public class adt {
 
-    Class string_class = "".getClass();
+
+    Object ok = (Object)new Object[]{"ok"};
+    Object error = (Object)new Object[]{"error"};
+
+    Class string_class;
+    Class object_class;
+
+    adt() {
+        try {
+            string_class = Class.forName("java.lang.String");
+            object_class = Class.forName("java.lang.Object");
+        }
+        catch (Throwable e) {
+            System.err.println(e);
+        }
+    }
 
     void write(Object o) {
         if (o.getClass() == string_class) {
@@ -22,18 +37,39 @@ public class adt {
             System.out.print(")");
         }
     }
-    void post(Object o) {
-        write(o);
-        System.out.print("\n");
+
+    Object post(Object o) {
+        try {
+            write(o);
+            System.out.print("\n");
+            return ok;
+        }
+        catch (Throwable e) {
+            System.err.println(e);
+            return error;
+        }
+    }
+    Object eval(Object o) {
+        try {
+            Object[] rec = (Object[])o;
+            String cmd = (String)rec[0];
+            Method m = this.getClass().getDeclaredMethod(cmd, new Class[] {object_class});
+            return m.invoke(this, rec[1]);
+        }
+        catch (Throwable e) {
+            System.err.println(e);
+            return error;
+        }
     }
 
     void go() {
         String[] s1 = new String[] {"a", "b", "c"};
-        Object o1 = new Object[]{"a", "b", new Object[]{"e", "f"}};
+        Object o1 = new Object[]{"post", new Object[]{"foo", "bar"}};
         Object o = (Object)s1;
         post(o);
         post("foo");
-
+        post(o1);
+        eval(o1);
     }
 
     public static void main(String[] args) {
