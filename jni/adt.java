48db4a3f1bd1c14cd979607d8bfb30396e382fdd
tom <tom@zwizwa.be>
2009-12-07 17:23:48 +0000

    eval stuff

--- jni/adt.java
+++ jni/adt.java
@@ -27,11 +27,14 @@ public class adt {
     final Object error = (Object)new Object[]{"error"};
     Class string_class;
     Class object_class;
+    // Class object_array_class;
     final Hashtable store = new Hashtable();
     adt() {
         try {
             string_class = Class.forName("java.lang.String");
             object_class = Class.forName("java.lang.Object");
+            // ????
+            // object_array_class = (Array.newInstance(object_class, 0)).getClass();
         }
         catch (Throwable e) {
             System.err.println(e);
@@ -137,20 +140,25 @@ public class adt {
         String[] s1 = new String[] {"a", "b", "c"};
         Object o1 = new Object[]{"post", new Object[]{"foo", "bar"}};
         Object o = (Object)s1;
-        post(o);
-        post("foo");
-        post(o1);
-        try { post(eval(o1)); }                     catch (Throwable e) {};
-        try { post(safe_apply(find("eval"), o1)); } catch (Throwable e) {};
-        try { post(find("eval")); }                 catch (Throwable e) {};
-        post(report);
-
-        put(arr("dummy", create(arr("dummy"))));
-        post(get(arr("dummy")));
 
 
-        put(arr("dummy", "foo"));
-        post(get(arr("dummy")));
+            // post(o);
+            // post("foo");
+            // post(o1);
+            // try { post(eval(o1)); }                     catch (Throwable e) {};
+            // try { post(safe_apply(find("eval"), o1)); } catch (Throwable e) {};
+            // try { post(find("eval")); }                 catch (Throwable e) {};
+            // post(report);
+            
+            // put(arr("dummy", create(arr("dummy"))));
+            // post(get(arr("dummy")));
+            
+            
+            // put(arr("dummy", "foo"));
+            // post(get(arr("dummy")));
+
+        try { post(eval(arr("post", "123"))); } catch (Throwable e) {};
+
     }
 
     public static void main(String[] args) {
