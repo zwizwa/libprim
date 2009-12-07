77cd3707b823c396a6644fb2c5026092a4549529
tom <tom@zwizwa.be>
2009-12-07 14:05:49 +0000

    Java reflection

---
+++ jni/reflect.java
@@ -0,0 +1,54 @@
+// package foo.bar;
+
+import java.lang.reflect.*;
+
+
+
+public class reflect {
+
+    String foo() { return "foo()"; }
+
+    int test1(int a, int b) { return a + b; }
+    Integer test2(Integer a, Integer b) { return a + b; }
+
+    void call1 ()
+    {
+        try {
+            Class int_class = Class.forName("java.lang.Integer");
+            Class this_class = this.getClass(); //Class.forName("reflect");
+            Class[] types = new Class[] {int_class, int_class};
+            Method m = this_class.getDeclaredMethod("test2", types);
+            Integer[] args = new Integer[] {1, 2};
+            Object result = m.invoke(this, args);
+            System.out.println((Integer)result);
+        }
+        catch (Throwable e) {
+            System.err.println(e);
+        }
+    }
+
+    void dump(String name)
+    {
+        try {
+            Class c = Class.forName(name);
+            Method m[] = c.getDeclaredMethods();
+            for (int i = 0; i < m.length; i++)
+                System.out.println(m[i].toString());
+        }
+        catch (Throwable e) {
+            System.err.println(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        reflect tn = new reflect();
+        // tn.dump(args[0]);
+        tn.call1();
+
+        // tn.test1(1, 2);
+        // Integer a = 1;
+        // Integer b = 2;
+        // tn.test2(a, b);
+        // tn.test1(a, b);
+    }
+}
--- jni/testnative.java
+++ jni/testnative.java
@@ -1,12 +1,43 @@
 
+import java.lang.reflect.*;
+
+class DumpMethods {
+    public static void main(String args[])
+    {
+        try {
+            Class c = Class.forName(args[0]);
+            Method m[] = c.getDeclaredMethods();
+            for (int i = 0; i < m.length; i++)
+                System.out.println(m[i].toString());
+        }
+        catch (Throwable e) {
+            System.err.println(e);
+        }
+    }
+}
+
+
 public class testnative {
+
+    void dump(String name)
+    {
+        try {
+            Class c = Class.forName(name);
+            Method m[] = c.getDeclaredMethods();
+            for (int i = 0; i < m.length; i++)
+                System.out.println(m[i].toString());
+        }
+        catch (Throwable e) {
+            System.err.println(e);
+        }
+    }
+
     public static void main(String[] args) {
         testnative tn = new testnative();
         byte buf[]  = tn.loadFile ("testnative.h");
 
-        for(int i=0;i<buf.length;i++) {
-            System.out.print((char)buf[i]);
-        }
+        // for(int i=0;i<buf.length;i++) System.out.print((char)buf[i]);
+        tn.dump("java.lang.String");
         
     }
     native byte[] loadFile(String name);
