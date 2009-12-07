6ca86885a272987fa9af0a60f9b78704eb661946
tom <tom@zwizwa.be>
2009-12-07 12:46:50 +0000

    jni example

--- jni/testnative.java
+++ jni/testnative.java
@@ -1,6 +1,14 @@
 
-
 public class testnative {
+    public static void main(String[] args) {
+        testnative tn = new testnative();
+        byte buf[]  = tn.loadFile ("testnative.h");
+
+        for(int i=0;i<buf.length;i++) {
+            System.out.print((char)buf[i]);
+        }
+        
+    }
     native byte[] loadFile(String name);
     static {
         // without "lib" prefix and ".so" suffix
