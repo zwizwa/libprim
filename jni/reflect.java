659c9b502e4150735bbc1fa0ab5df5b160d2a77d
tom <tom@zwizwa.be>
2009-12-09 09:23:25 +0000

    reflect

--- jni/reflect.java
+++ jni/reflect.java
@@ -9,6 +9,8 @@ public class reflect {
     String foo() { return "foo()"; }
 
     int test1(int a, int b) { return a + b; }
+    // void test1(int a, int b) { }
+
     Integer test2(Integer a, Integer b) { return a + b; }
 
     void call1 ()
@@ -16,9 +18,16 @@ public class reflect {
         try {
             Class int_class = Class.forName("java.lang.Integer");
             Class this_class = this.getClass(); //Class.forName("reflect");
-            Class[] types = new Class[] {int_class, int_class};
+
+            // Class[] types = new Class[] {int_class, int_class};
+            // Method m = this_class.getDeclaredMethod("test2", types);
+            
+            Class[] types = new Class[] {Integer.TYPE, Integer.TYPE};
             Method m = this_class.getDeclaredMethod("test1", types);
-            Integer[] args = new Integer[] {1, 2};
+            
+
+
+            Object[] args = new Object[] {1, 2};
             Object result = m.invoke(this, args);
             System.out.println((Integer)result);
         }
@@ -32,16 +41,8 @@ public class reflect {
         try {
             Class c = Class.forName(name);
             Method m[] = c.getDeclaredMethods();
-            for (int i = 0; i < m.length; i++) {
-                // System.out.println(m[i].toString());
-                System.out.println(m[i].getName());
-                Class[] cs = m[i].getParameterTypes();
-                for (int j = 0; j < cs.length; j++) {
-                    System.out.println("   " + cs[j].getName());
-                }
-                System.out.println("-> " + m[i].getReturnType().getName());
-                System.out.println("");
-            }
+            for (int i = 0; i < m.length; i++)
+                System.out.println(m[i].toString());
         }
         catch (Throwable e) {
             System.err.println(e);
@@ -51,7 +52,7 @@ public class reflect {
     public static void main(String[] args) {
         reflect tn = new reflect();
         tn.dump(args[0]);
-        // tn.call1();
+        tn.call1();
 
         // tn.test1(1, 2);
         // Integer a = 1;
