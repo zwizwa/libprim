379cb2dd637f61daa6d506d181cf667379766946
tom <tom@zwizwa.be>
2009-12-09 17:57:03 +0000

    jni consoleEvalString ok

--- jni/sc.java
+++ jni/sc.java
@@ -1,6 +1,6 @@
 public class sc {
     /* Native methods */
-    public native String eval(String commands);
+    public native static String consoleEvalString(long console, String commands);
 
     /* Initialize the VM.  Returns a pointer cast as a long. */
     public native static long boot(String bootfile);
@@ -27,6 +27,14 @@ public class sc {
     public native static long prepareConsoleServer(long vm, String node);
 
 
+    /* Spawn vm in thread */
+    public static Thread spawnResume(final long vm) {
+        Thread t = new Thread() { public void run() { sc.resume(vm); }};
+        t.start(); 
+        return t;
+    }
+
+
     /* load the binary lib on application startup. */
     static {
         System.loadLibrary("sc");
@@ -35,6 +43,11 @@ public class sc {
     public static void main(String[] arg) {
         long vm = boot("../sc/boot-expanded.scm");
         long console = prepareConsoleServer(vm, "/tmp/sc");
-        resume(vm);
+        // resume(vm);
+        spawnResume(vm);
+
+        String rv = consoleEvalString(console, "(+ 1 2)");
+
+        System.out.println("RV: " + rv);
     }
 }
