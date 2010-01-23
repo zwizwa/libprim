3ed9a9fded76ca9531c9dd488145eb29db98a909
tom <tom@zwizwa.be>
2010-01-23 09:23:16 +0000

    build targets -> module.mk

--- sc_vm2/module.mk
+++ sc_vm2/module.mk
@@ -1,22 +1,25 @@
+# Sources for $(LOCAL_MODULE).a
 LOCAL_OBJ=vm2.o
 
+# Define global application target.
+SC_VM2 := $(LOCAL_MODULE)/vm2
+$(call app_rule, $(SC_VM2), sc_vm2 sc ex leaf, -lpthread)
 
-# All global rules created inside a module.mk that use the MODULE
-# variable need to expand it using ":=" before the end of the file.
-BOOT:=sc_vm1/boot12.scm_
-VM_TEST_DEPS := $(LOCAL_MODULE)/vm2 $(BOOT)
+# Boot file comes from VM1
+SC_VM2_BOOT := sc_vm1/boot12.scm_
+VM_TEST_DEPS = $(SC_VM2) $(SC_VM2_BOOT)
 
 # The test is run in the source directory to have easy access to .scm
-# files from the console.
+# files from the console.  (Since this rule depends on LOCAL_MODULE
+# it's stored in a variable.)
 VM_TEST_CMD := \
 	cd $(SRCDIR)/$(LOCAL_MODULE); \
 	gdb -x $(SRCDIR)/bin/run.gdb \
-		--args $(BUILDDIR)/$(LOCAL_MODULE)/vm2 \
-		--boot $(BUILDDIR)/$(BOOT) $(VM_TEST_EVAL) \
+		--args $(BUILDDIR)/$(SC_VM2) \
+		--boot $(BUILDDIR)/$(SC_VM2_BOOT) \
 		--eval '(begin (load "macros.scm") (load "compile.scm") (repl))'
 
-.PHONY: vm_test vm_all
-
+.PHONY: sc_vm2_all sc_vm2_test
 sc_vm2_all: $(VM_TEST_DEPS)
 
 sc_vm2_test: $(VM_TEST_DEPS)
