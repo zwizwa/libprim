6d5b9a45d3552e7f990074e5576d6177b02890bf
tom <tom@zwizwa.be>
2010-01-24 10:39:48 +0000

    vm2 restored

--- sc_vm2/module.mk
+++ sc_vm2/module.mk
@@ -6,8 +6,7 @@ $(call app_rule,sc_vm2, ex leaf, -lpthread)
 
 # Boot file comes from VM1
 SC_VM2       := $(LOCAL_MODULE)/sc_vm2
-SC_VM2_BOOT  := sc_vm1/boot12.scm_
-SC_VM2_TEST_DEPS := $(SC_VM2) $(SC_VM2_BOOT)
+SC_VM2_BOOT  := $(SRCDIR)/sc_vm1/boot.scm
 
 # The test is run in the source directory to have easy access to .scm
 # files from the console.  (Since this rule depends on LOCAL_MODULE
@@ -16,12 +15,10 @@ SC_VM2_TEST_CMD := \
 	cd $(SRCDIR)/$(LOCAL_MODULE); \
 	gdb -x $(SRCDIR)/bin/run.gdb \
 		--args $(BUILDDIR)/$(SC_VM2) \
-		--boot $(BUILDDIR)/$(SC_VM2_BOOT) \
+		--boot $(SC_VM2_BOOT) \
 		--eval '(begin (load "vm1vm2.scm") (repl))'
 
-.PHONY: sc_vm2_all sc_vm2_test
-sc_vm2_all: $(SC_VM2_TEST_DEPS)
-
-sc_vm2_test: $(SC_VM2_TEST_DEPS)
+.PHONY: sc_vm2_test
+sc_vm2_test: $(SC_VM2)
 	$(SC_VM2_TEST_CMD)
 
