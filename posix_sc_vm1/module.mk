d3c9e02378c6b1754b7d1cb75f194363e9ad181a
tom <tom@zwizwa.be>
2010-09-18 21:12:06 +0000

    cleanup

--- posix_sc_vm1/module.mk
+++ posix_sc_vm1/module.mk
@@ -7,10 +7,12 @@ TARGET_MODULES := sc_vm1 sc ex_posix ex leaf_posix leaf
 TARGET_LDFLAGS := -lpthread
 
 # Test target.
-.PHONY: sc_vm1_posix_test
+
+
+.PHONY: posix_sc_vm1
 SC_VM1_POSIX := $(BUILDDIR)/$(MODULE)/sc_vm1_posix
 SC_BOOT_SCM := $(SRCDIR)/sc_vm1/boot.scm
-sc_vm1_posix_test: $(SC_VM1_POSIX)
+posix_sc_vm1: $(SC_VM1_POSIX)
 	gdb -x $(SRCDIR)/bin/run.gdb --args $(SC_VM1_POSIX) --boot $(SC_BOOT_SCM)
 
 
