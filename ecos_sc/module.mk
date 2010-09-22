ada85caa4eb0d0173b8cd3fcb01fa10e032a755d
tom <tom@zwizwa.be>
2010-09-22 10:01:39 +0000

    ecos/module.mk

---
+++ ecos_sc/module.mk
@@ -0,0 +1,14 @@
+# Define objects in this module
+MODULE_OBJ := main.o
+
+# Define a new global build target.
+TARGET         := main
+TARGET_MODULES := sc_vm1 sc ex leaf
+
+# Test target.
+SC_VM1_ECOS := $(BUILDDIR)/$(MODULE)/main
+.PHONY: ecos_sc
+ecos_sc: $(SC_VM1_ECOS)
+	gdb -x $(SRCDIR)/bin/run.gdb --args $(SC_VM1_ECOS)
+
+
