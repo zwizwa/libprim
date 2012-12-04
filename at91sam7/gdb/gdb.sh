e470a9bf5446ff31b80b5b9b11bc630d3f603667
tom <tom@zwizwa.be>
2012-12-04 16:44:54 +0000

    notes

--- at91sam7/gdb/gdb.sh
+++ at91sam7/gdb/gdb.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 GDB=arm-eabi-gdb-7.2  # YMMV
-exec $GDB -x $(dirname $0)/init.gdb "$@"
+exec $GDB -x $(dirname $0)/util.gdb -x $(dirname $0)/init.gdb "$@"
--- at91sam7/gdb/init.gdb
+++ at91sam7/gdb/init.gdb
@@ -1,6 +1,4 @@
 # -*- conf -*-
 # In emacs: M-x gdb arm-eabi-gdb-7.2 --annotate=3
-source util.gdb
 connect
-
 
