ee57abfb6d535c4c9b0d9b79dccc02f52cd05edf
tom <tom@zwizwa.be>
2012-12-05 02:02:24 +0000

    toolchain stuff

--- download.mk
+++ download.mk
@@ -1,11 +1,24 @@
 # Given variable URLS, generate download rules and define a variable FILES.
 FILES := $(notdir $(URLS))
+UNPACKS := $(patsubst %.tar.gz,%/.unpack,$(FILES))
+
 download: $(FILES)
+
+download_clean:
+	rm -f $(FILES)
+
+%/.unpack: %.tar.gz
+	tar xf $<
+	touch $@
+
+unpack: $(UNPACKS)
+unpack_clean:
+	rm -rf $(dir $(UNPACKS))
+
 define download_template
 $(1):
 	wget --passive-ftp $(2)
 endef
 $(foreach url,$(URLS),$(eval $(call download_template,$(notdir $(url)),$(url))))
 
-download_clean:
-	rm -f $(FILES)
+
