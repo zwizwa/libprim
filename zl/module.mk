e006ac178410e1842f3f5def6637e99dc0b0a16a
tom <tom@zwizwa.be>
2014-08-16 16:55:05 +0000

    build fixes

--- zl/module.mk
+++ zl/module.mk
@@ -1,3 +1,3 @@
 m_C   := ffmpeg.c glx.c gsl.c sc_wrap.c v4l.c xv.c xwindow.c yuv.c layout2D.c
-m_ELF := test_v4l_xv.elf
+m_ELF := # test_v4l_xv.elf
 m_LDFLAGS := -lpthread -L/usr/X11R6/lib -lX11 -lXv -lXext -lGL -lGLU
